0b075ca6836d38e88cfcc9e8652bcaac4145002f
TolaSoy <tolasmartboy.soy@gmail.com>
2021-07-27 22:10:12 +0700

    create route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('/products', ProductController::class);
+Route::group(["prefix" => "products"], function() {
+    Route::apiResource('/{productId}/reviews', ReviewController::class);
 });
